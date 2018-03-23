a069f1ffc28040ac68e37a2be0291e523f21b93d
Roberto Gian Franco Carbonell Saravia <r.gian.f.carbonell.s@gmail.com>
2018-03-22 20:14:45 -0500

    Add Link E-Mail -> Aside}

--- static/js/dev/web-aside/components/system-aside.js
+++ static/js/dev/web-aside/components/system-aside.js
@@ -12,7 +12,7 @@ class SystemAside extends React.Component {
   render (){
     return (
         <aside className="menu bar-system right-0 ">
-            <a classame="center-align">  <img src={require(`./../../../../media/images/png/EMail-Green-1.png`)}  alt="E-Mail"/> </a>
+            <a classame="center-align" target="_blank" href="http://webmail.qygconsultores.com/Mondo/lang/sys/login.aspx">  <img src={require(`./../../../../media/images/png/EMail-Green-1.png`)}  alt="E-Mail"/> </a>
             <a classame="center-align">  <img src={require(`./../../../../media/images/png/Control-Panel-1.png`)}  alt="Control-Panel"/> </a>
         </aside>
     )
