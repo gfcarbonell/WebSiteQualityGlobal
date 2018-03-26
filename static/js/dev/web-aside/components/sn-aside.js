511464c4f009c48f7310dc5f5f03a22ad86ffe5f
Roberto Gian Franco Carbonell Saravia <r.gian.f.carbonell.s@gmail.com>
2018-03-26 17:05:07 -0500

    Inserted Link FansPasge Quality Global

--- static/js/dev/web-aside/components/sn-aside.js
+++ static/js/dev/web-aside/components/sn-aside.js
@@ -12,7 +12,8 @@ class SNAside extends React.Component {
   render (){
     return (
         <aside className="menu bar-sn top-25">
-            <a classame="center-align">  <img src={require(`./../../../../media/images/svg/Facebook.svg`)}  alt="Facebook"/> </a>
+            <a href="https://www.facebook.com/qualityAsesoresyconsultores/" target="_blank"
+               classame="center-align">  <img src={require(`./../../../../media/images/svg/Facebook.svg`)}  alt="Facebook"/> </a>
             <a classame="center-align">  <img src={require(`./../../../../media/images/svg/Twitter.svg`)}  alt="Twitter"/> </a>
             <a classame="center-align">  <img src={require(`./../../../../media/images/svg/Youtube.svg`)}  alt="Youtube"/> </a>
             <a classame="center-align">  <img src={require(`./../../../../media/images/svg/Vimeo.svg`)}  alt="Vimeo"/> </a>
